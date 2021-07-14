834073f5831c0eaa27fcbb55c6f723b3a3a11521
Flo ubuntu 20 <hortala.florent@hotmail.Fr>
2021-07-14 02:10:54 -0700

    Ajoue du switch pour les droits des utilisateurs

--- js/Admin/PanneauAdmin/InformationUtilisateur.js
+++ js/Admin/PanneauAdmin/InformationUtilisateur.js
@@ -47,12 +47,10 @@ function ResquestRequestGetInfosAllUserSuccess(response) {
         let tdUseId = $("<td>").attr('hidden', 'true').text(response[i].use_id)
         let tdUseName = $("<td>").text(response[i].use_name)
         let tdUseMail = $("<td>").text(response[i].use_mailadress)
-        let tdUseDroit = $("<td>").html(response[i].use_droit + " <button type=button class=\"btn  btn-primary btn-visualisationUserFood\" id=visualisationUserFood_"+ response[i].use_id +" onclick=VisualisationFoodUser(" + response[i].use_id + ")>-></td>")
+        let tdUseDroit = $("<td>").html(response[i].use_droit + " <button type=button class=\"btn  btn-primary btn-visualisationUserFood\" id=SwitchDroitUserID_"+ response[i].use_id +" onclick=SwitchDroitUser(" + response[i].use_id + ")>-></td>")
         let tdVisualisation = $("<td>").html("<button type=button class=\"btn  btn-primary btn-visualisationUserFood\" id=visualisationUserFood_"+ response[i].use_id +" onclick=VisualisationFoodUser(" + response[i].use_id + ")>?</td>")
         trInfosUtilisateurBody.append(tdUseId, tdUseName, tdUseMail, tdUseDroit, tdVisualisation)
         tBodyInfosUtilisateur.append(trInfosUtilisateurBody)
-        
-        //$("#cardBodyPanneauAdminID").append(labelUseId, labelUseName, labelUseMail, labelUseDroit)
     }
 
     tableInfosUtilisateur.append(tHeadInfosUtilisateur, tBodyInfosUtilisateur)
@@ -76,11 +74,60 @@ function ResquestRequestGetInfosAllUserError(request, status, error) {
 }
 /*End get information*/
 
+/* Begin switch droit user */
+
+function RequestSwitchUserDroit( userIDSwitch, destination) {
+    $.ajax({
+        type: "POST",
+        url: url + port + destination,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: `{"UserIdSwitch": "${userIDSwitch}"}`,
+        asynch: true,
+        success: function(response) {
+            RequestSwitchUserDroitSuccess(response);
+        },
+        error: function(request, status, error) {
+            RequestSwitchUserDroitError(request, status, error);
+        }
+    });
+}
+
+function RequestSwitchUserDroitSuccess(response) {
+    window.location.reload(true); 
+}
+
+function RequestSwitchUserDroitError(request, status, error) {
+    $("#btnAddFreezer").prop('disabled', false);
+
+    if (request.status == 570) {
+        alert("Nom du frigo déjà utilisé");
+    } else if (request.status == 550) {
+        alert("Problème base de donnée");
+    } else if (request.status == 520) {
+        alert("Utilisateur inexistant");
+    } else if (request.status == 400) {
+        alert("Bad format");
+    } else {
+        alert("Error");
+    }
+}
+
+/* End switch droit user */
+
 function VisualisationFoodUser(userID){
     $("#visualisationUserFood_" + userID).attr('disabled', 'true')
 
     alert("Id de l'utilisateur : " + userID)
 
+}
+
+function SwitchDroitUser(userIDSwitch){
+    $("#SwitchDroitUserID_" + userIDSwitch).attr('disabled', 'true')
+
+    alert("id de l'utilisateur switch = " + userIDSwitch)
+
+    RequestSwitchUserDroit(userIDSwitch, sessionStorage.getItem("idUser") + "/switchDroitUser")
 }
 
 
